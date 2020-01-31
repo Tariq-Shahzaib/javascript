7935dfffb8b5254377b84588bc58e9f6320574b8
Tariq-Shahzaib <Shahzaibzaibi001@gmail.com>
2020-01-31 17:52:20 +0100

    ssss

--- calculator.js
+++ calculator.js
@@ -1,51 +1,68 @@
-
+var button = document.getElementById("calculate")
 console.log("Basic Calculator");
-do{
-
-var fp = prompt("Please enter first number");
-var op = prompt("Please enter the oprand +, -, *, /");
-var sp = prompt("Please enter second number"); 
-
-var rs=0;
-var fn=Number(fp);
-var sn=Number(sp);
-//if((Number.isnumber(fn) == true) && (Number.isInteger(sn) == true ))
-if((Number.isNaN(fn) === false) && (Number.isNaN(sn) === false ))
+button.onclick=
+function calculate()
 {
+    console.log('click work')
+    var fn = document.getElementById("num1").value;
+    var op = document.getElementById("option").value;
 
-    
-    switch (op)
-    {
-        case '+':
-            rs = fn + sn;
-            console.log (rs);
-            break;
+var sn = document.getElementById("num2").value;
+
+fn=Number(fn);
+sn=Number(sn);
+
+        /*var fp = prompt("Please enter first number");
+        var op = prompt("Please enter the oprand +, -, *, /");
+        var sp = prompt("Please enter second number"); 
+        
+        var rs=0;
+        var fn=Number(fp);
+        var sn=Number(sp);*/
+        //if((Number.isnumber(fn) == true) && (Number.isInteger(sn) == true ))
+        if((Number.isNaN(fn) === false) && (Number.isNaN(sn) === false ))
+        {
+  
             
-            case '-':
-            rs = fn - sn;
-            console.log (rs);
-            break;
+            switch (op)
+            {
+                case '+':
+                    rs = fn + sn;
+                    console.log (rs);
+                    break;
+                    
+                    case '-':
+                    rs = fn - sn;
+                    console.log (rs);
+                    break;
+        
+                    case '*':
+                    rs = fn * sn;
+                    console.log (rs);
+                    break;
+        
+                    case '/':
+                    rs = fn / sn;
+                    console.log (rs);
+                    break;
+                default:
+                    console.log('You Entered a invalid Operand...');
+                    break;
+                
+            }    
+            var result = document.getElementById("output");
+            result.value = rs;
+           
+        }
+        else 
+        {
+            console.log ('You must have to enter number')
+        }
+        
+        
+        
+        
+        
 
-            case '*':
-            rs = fn * sn;
-            console.log (rs);
-            break;
-
-            case '/':
-            rs = fn / sn;
-            console.log (rs);
-            break;
-        default:
-            console.log('You Entered a invalid Operand...');
-            break;
-    }    
-   var ex = prompt("press 1 for Continue Or press any other key to Exit the Program...");
 }
-else 
-{
-    console.log ('You must have to enter number')
-}
-} while (ex==1);
-
-
 
