16e8ca75586cbdc968e6051ab1eb96b07516eefd
Tariq-Shahzaib <Shahzaibzaibi001@gmail.com>
2020-02-04 14:23:56 +0100

    Three Loops got Toghe ther

--- loops.js
+++ loops.js
@@ -1,23 +1,43 @@
+                // While Loop
+ const NA = "Na ";
+let i=0;
+let lastWord = 'Batman!';
+let word = NA;
+ //jointWorld=word;
+ while (i <= 16) {
+   word+= NA;
+   i++;
+ }
+ console.log(word+lastWord)
 
 
+
+
+
+                 // For Loop
+
+ const NA = "Na ";
+ let lastWord = 'Batman!';
+ let word = NA;
+ for (let i=0;i<=16;i++){
+     word += NA;
+     }
+ console.log(word+lastWord)
+
+
+
+
+
+
+                //Do While Loop
+let i=0;
 
 const NA = "Na ";
-let i = 1;
+let lastWord = 'Batman!';
 let word = NA;
-//let jointWorld="";
-let lastWord = 'Batman!'
-//jointWorld=word;
-while (i <= 16) {
-  word += NA;
-  i++;
-}
+do{
+    word += NA;
+    i++;
+    }
+    while (i<=16);
 console.log(word+lastWord)
-
-
-
-
-
-
-while (i<=16) {
-
-}
