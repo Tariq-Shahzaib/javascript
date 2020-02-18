b7855738ad6475cf7b7901714fc614dd74db7404
Tariq-Shahzaib <Shahzaibzaibi001@gmail.com>
2020-02-18 17:48:45 +0100

    Ascii Button with Hello inside

--- asciibutton.js
+++ asciibutton.js
@@ -7,32 +7,22 @@ ASCII Button
 *       *
 *********
 */
-let word= 'hello'
-let star='*'
-    for(let lines=1;lines<6;lines++)
-    {
-        let columns=''
-        let line=''
-        for(let columns=1;columns<=word.length+4;columns++)
-        
-        {       
-            if(lines==2 || lines==3 || lines==4)
-            {       
-                if(lines===2 || lines===4)
-                {
-                    line+='*       *' 
-                    break;
-                }
-            }  
-           
-            else
-            {
-                line+=star 
-            }     
-        }
-        if(lines==3 )
-        {
-         line+='* '+word+' *' 
-        }         
-        console.log(line)  
+let word = "hello";
+let star = "*";
+for (let lines = 1; lines < 6; lines++) {
+  let line = "";
+  for (let columns = 1; columns <= word.length + 4; columns++) {
+    if (lines == 2 || lines == 3 || lines == 4) {
+      if (lines === 2 || lines === 4) {
+        line += "*       *";
+        break;
+      }
+    } else {
+      line += star;
     }
+  }
+  if (lines == 3) {
+    line += "* " + word + " *";
+  }
+  console.log(line);
+}
